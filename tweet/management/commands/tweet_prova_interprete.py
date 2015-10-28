473377d3da4aa1335c9ae658df1eae727fb19d4e
caipirginka <github@oicom.com>
2015-10-28 17:30:42 +0100

    Creato management command per fare prove di interpretazione dei tweet sulle istanze già presenti nel database.

---
+++ tweet/management/commands/tweet_prova_interprete.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime, timedelta
+import os
+import re
+import sys
+
+from django.core.files.base import ContentFile
+from django.core.management.base import NoArgsCommand, CommandError
+from django.db import transaction
+
+from tweet.models import Tweet
+
+
+def tweet_interpreter(dict_tweet):
+    #normalizza con valori di default
+    dict_tweet.setdefault('tipo',0)    
+    dict_tweet.setdefault('stamp',datetime.now().isoformat())    
+    # TODO: verificare se necessario filtrare i twitter con reply !=0 per eliminare i tweet di risposta ad utenti #if not row['reply']
+    # TODO: verificare se necessario filtrare i RT
+    testo = dict_tweet['testo']    
+    ### se un tweet inizia con un numero viene escluso perchè contiene info su date future
+    if re.match(r'(\d)',testo[0]):
+        return [dict_tweet]
+    ### se un tweet ha l'hashtag #Milan viene escluso perchè è in inglese (non sempre il filtro sulla lingua funziona)
+    if re.search(r'\bMilan\b',testo):
+        return [dict_tweet]
+    ### se un tweet non contiene almeno un ":", viene escluso
+    before,sep,after = testo.partition(':')
+    if not sep or not after:
+        return [dict_tweet]
+    ### prova ad estrarre dal testo prima dei ":" i riferimenti alle Linee
+    linee = re.findall (r'(#bus\d+|#tram\d+|#M\d)', before)
+    ### se un tweet non ha riferimenti alle Linee, viene escluso
+    if not linee:
+        return [dict_tweet]
+    ### si deve creare un evento per ogni Linea alla quale il Tweet fa riferimento      
+    interpreted_tweet=[]
+    for linea in linee:
+        d = {'stamp': dict_tweet['stamp'],
+             'testo': dict_tweet['testo'],
+             }
+        #in codice Linea separa testo e numero e sostituisce con singolo carattere (Es: 'bus58'=>'B58')
+        m = re.search(r'\#(\w+)(\d+)',linea)    
+        d['linea'] = m.group(1).replace('bus','B').replace('tram','T') + m.group(2)            
+        # tipo [0 = non evento, 1 = aperto, 2 = continuazione, 3 = chiuso]
+        if re.search(r'riprend|prosegue',after):
+            d['tipo'] = 3         #evento di Chiusura
+        else:                 ### non è gestito l'evento continuazione: di default sono tutti di apertura = 1            
+            d['tipo'] = 1         #evento di Apertura
+        interpreted_tweet.append(d)
+    return interpreted_tweet
+
+def do_it():
+#   tweet = Tweet.objects.get(pk=6137)
+  for tweet in Tweet.objects.exclude(testo__iregex=r'^RT'):#.exclude(tipo=1):
+    res = tweet_interpreter({'testo': tweet.testo, 'stamp': tweet.stamp})
+    for t in res:
+#       if 'linea' in t:
+      tipo = t.get('tipo',0) 
+      if tipo != tweet.tipo: 
+        print u"{} => {} {}".format(tweet.tipo,tipo,t)
+  
+class Command(NoArgsCommand):
+  help = 'Prova interprete Tweet'
+  
+  def printline(self,msg):
+    self.stdout.write(u'{}: {}\n'.format(msg,self.help))
+    
+  @transaction.atomic
+  def handle_noargs(self, **options):
+    self.printline('Inizio')
+#     transaction.commit_unless_managed()
+#     transaction.enter_transaction_management()
+#     transaction.managed(True)
+    try:
+      do_it()
+#       transaction.commit()
+      self.printline('Fine')
+    except Exception, exc:
+#       transaction.rollback()
+      raise CommandError(self.help + "\n" + str(exc))
+#     finally:
+#       transaction.leave_transaction_management()
