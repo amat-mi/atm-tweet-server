5940cbab4fb80aeae8a1ddcf3d50d3350d8611db
caipirginka <github@oicom.com>
2018-05-29 17:23:32 +0200

    Aggiornato per Django 2.0 (nuova versione di Django Rest Swagger).

--- atmtweet/urls.py
+++ atmtweet/urls.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from django.conf import settings
-from django.urls import re_path as url, include
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import re_path as url, include
 from django.views.generic.base import RedirectView
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework_swagger.views import get_swagger_view
 
 
 urlpatterns = [
@@ -15,7 +16,6 @@ urlpatterns = [
 
     url(r'^admin/', admin.site.urls),
     url(r'^tweet/', include('tweet.urls')),
-    url(r'^docs/', include('rest_framework_swagger.urls')),
 
     # Questa è l'URL per la UI Client (in remoto gestita direttamente dal Web Server)
     url(r'^client/', RedirectView.as_view(url='/static/index.html', permanent=False), name="client"),
@@ -23,6 +23,9 @@ urlpatterns = [
 
 urlpatterns = format_suffix_patterns(urlpatterns)
 
+urlpatterns = [
+    url(r'^docs/', get_swagger_view(title='AMAT ATM Tweet API'))
+]
 
 urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
