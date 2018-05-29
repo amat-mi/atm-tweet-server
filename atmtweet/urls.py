d75378b5ee1a7b0897de813fe3db7d5d345b81df
caipirginka <github@oicom.com>
2018-05-29 17:22:01 +0200

    Aggiornato per Django 2.0 (rimosso patterns e nuovo import di url).

--- atmtweet/urls.py
+++ atmtweet/urls.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
 
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.urls import re_path as url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic.base import RedirectView
 from rest_framework.urlpatterns import format_suffix_patterns
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'atmtweet.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^tweet/', include('tweet.urls')),
     url(r'^docs/', include('rest_framework_swagger.urls')),
 
     # Questa è l'URL per la UI Client (in remoto gestita direttamente dal Web Server)
     url(r'^client/', RedirectView.as_view(url='/static/index.html', permanent=False), name="client"),
-)
+]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
 
