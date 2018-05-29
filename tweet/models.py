98f33b21dc948401e6e74de76610be92fb031e01
caipirginka <github@oicom.com>
2018-05-29 17:12:15 +0200

    Aggiornato per Django 2.0 (corretto utilizzo di max_length nei models).

--- tweet/models.py
+++ tweet/models.py
@@ -13,15 +13,16 @@ class Tweet (models.Model):
         (2, 'Continuazione'),
         (3, 'Chiusura'),
 	)
-	tipo = models.IntegerField(max_length=1, choices=TIPO, null=False,blank=False)
+	tipo = models.IntegerField(choices=TIPO, null=False,blank=False)
 	stamp = models.DateTimeField(auto_now=False, null=False,blank=False)
 	stamp_evento = models.DateTimeField(auto_now=False, null=True,blank=True)
 	root_tweet = models.ForeignKey('self', null=True,blank=True, on_delete=models.CASCADE)
-	linea = models.CharField(max_length='50', null=True,blank=True)
-	causa = models.CharField(max_length='100', null=True,blank=True)
-	testo = models.CharField(max_length='250', null=False,blank=False)
+	linea = models.CharField(max_length=50, null=True,blank=True)
+	causa = models.CharField(max_length=100, null=True,blank=True)
+	testo = models.CharField(max_length=250, null=False,blank=False)
 
 	class Meta: # creare la classe meta per sistemare il plurale dell admin
 		verbose_name_plural = "Tweet rilevati dallo stream di ATM"
+		
 	def __unicode__(self):
 		return u'%s_%s_%s' %(self.stamp, self.tipo, self.linea)
