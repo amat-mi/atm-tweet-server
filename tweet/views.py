7c5546d009fa1446f031944f3cdb3bad02f75b13
caipirginka <github@oicom.com>
2018-06-06 13:10:54 +0200

    Aggiornato per Django 2.0 (usare query_params invece di REQUEST).

--- tweet/views.py
+++ tweet/views.py
@@ -34,7 +34,7 @@ class TweetViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         res = super(TweetViewSet,self).get_queryset()
-        last_pk = self.request.REQUEST.get('last_pk',None)
+        last_pk = self.request.query_params.get('last_pk',None)
         return res.filter(pk__gt=last_pk) if last_pk else res
 
     @list_route(methods=['PUT'])
